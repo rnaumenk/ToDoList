54c8dae6a35b59e72b5d8926dce113a03cc5d288
Ruslan Naumenko <>
2019-07-12 12:57:23 +0300

    first

---
+++ ToDoList/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ToDoList
+//
+//  Created by Ruslan on 7/12/19.
+//  Copyright © 2019 Ruslan Naumenko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
